9b4d326ffd46f72ee7768a43ed3584e1bc9f1f84
Andrew Meyer <ameyer@zerosla.com>
2026-04-30 15:44:09 -0500

    attempt to fix blocking0

--- components/zmpt101b/zmpt101b.cpp
+++ components/zmpt101b/zmpt101b.cpp
@@ -7,42 +7,40 @@ namespace zmpt101b {
 
 static const char *const TAG = "zmpt101b";
 
-int ZMPT101BSensor::getZeroPoint_() {
-	uint32_t Vsum = 0;
-	uint32_t measurements_count = 0;
-	uint32_t t_start = micros();
-
-	while (micros() - t_start < this->period_) {
-		Vsum += adc_sensor_->sample() * ADC_SCALE;
-		measurements_count++;
-	}
-
-	return measurements_count ? (Vsum / measurements_count) : 0;
-}
-
 void ZMPT101BSensor::loop() {
-	double readingVoltage = 0.0;
+	uint32_t now = micros();
 
-	for (uint8_t i = 0; i < this->loop_count_; i++) {
-		int zeroPoint = this->getZeroPoint_();
+	if (count_ == 0)
+		t_start_ = now;
 
-		int32_t Vnow = 0;
-		uint32_t Vsum = 0;
-		uint32_t measurements_count = 0;
-
-		uint32_t t_start = micros();
-		while (micros() - t_start < this->period_) {
-			Vnow = (adc_sensor_->sample() * ADC_SCALE) - zeroPoint;
-			Vsum += Vnow * Vnow;
-			measurements_count++;
+	if (state_ == State::CALIBRATING) {
+		if (now - t_start_ < period_) {
+			vsum_ += adc_sensor_->sample() * ADC_SCALE;
+			count_++;
+		} else {
+			zero_point_ = count_ ? (vsum_ / count_) : 0;
+			vsum_ = 0;
+			count_ = 0;
+			state_ = State::MEASURING;
 		}
-
-		double v_out_rms = sqrt((double)Vsum / (double)measurements_count) / ADC_SCALE * VREF;
-		double mains_v_rms = v_out_rms * (1000.0 / this->sensitivity_);
-		readingVoltage += mains_v_rms;
+	} else {
+		if (now - t_start_ < period_) {
+			int32_t v = (adc_sensor_->sample() * ADC_SCALE) - zero_point_;
+			vsum_ += v * v;
+			count_++;
+		} else {
+			double v_rms = sqrt((double) vsum_ / count_) / ADC_SCALE * VREF;
+			accumulated_v_ += v_rms * (1000.0 / sensitivity_);
+			vsum_ = 0;
+			count_ = 0;
+			if (++iteration_ >= loop_count_) {
+				publish_state(accumulated_v_ / loop_count_);
+				accumulated_v_ = 0.0;
+				iteration_ = 0;
+			}
+			state_ = State::CALIBRATING;
+		}
 	}
-
-	this->publish_state(readingVoltage / this->loop_count_);
 }
 
 }  // namespace zmpt101b
--- components/zmpt101b/zmpt101b.h
+++ components/zmpt101b/zmpt101b.h
@@ -20,12 +20,20 @@ class ZMPT101BSensor : public sensor::Sensor, public Component {
   void loop() override;
 
  protected:
+  enum class State { CALIBRATING, MEASURING };
+
   adc::ADCSensor *adc_sensor_;
   uint32_t period_;
   float sensitivity_;
   int loop_count_;
 
-  int getZeroPoint_();
+  State state_{State::CALIBRATING};
+  uint32_t t_start_{0};
+  uint32_t vsum_{0};
+  uint32_t count_{0};
+  int32_t zero_point_{0};
+  double accumulated_v_{0.0};
+  uint8_t iteration_{0};
 };
 
 }  // namespace zmpt101b
